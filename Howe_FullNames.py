3925583ff5f689cbafc2e3e3ce80afccce38963a
Jaylen Howe <168600994+Jayh15@users.noreply.github.com>
2025-03-06 20:54:38 -0500

    Add files via upload

---
+++ Howe_FullNames.py
@@ -0,0 +1,7 @@
+def full_name(first_name, last_name):
+    print(f"Hello, {first_name} {last_name}.")
+
+# Calling the function three times with different names
+full_name("Kevin", "James")
+full_name("Jane", "Smith")
+full_name("Emily", "Johnson")
